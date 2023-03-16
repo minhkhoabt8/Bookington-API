44e2a7df52995f0ad186522361026416f6e69563
Khoa Le <minhkhoabtr8@gmail.com>
2023-03-17 00:43:20 +0700

    Create Time Mapping

---
+++ Bookington.Infrastructure/Enums/DaysOfWeekEnum.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bookington.Infrastructure.Enums
+{
+    public class DaysOfWeekEnum
+    {
+        public enum DaysOfWeek
+        {
+            None = 0,
+            Monday = 1 << 0,
+            Tuesday = 1 << 1,
+            Wednesday = 1 << 2,
+            Thursday = 1 << 3,
+            Friday = 1 << 4,
+            Saturday = 1 << 5,
+            Sunday = 1 << 6,
+            Weekdays = Monday | Tuesday | Wednesday | Thursday | Friday,
+            Weekends = Saturday | Sunday,
+            All = Weekdays | Weekends
+        }
+
+        
+        
+    }
+}
--- Bookington.Infrastructure/Services/Implementations/BookingService.cs
+++ Bookington.Infrastructure/Services/Implementations/BookingService.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Diagnostics;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿
 using AutoMapper;
 using Bookington.Core.Entities;
 using Bookington.Core.Exceptions;
